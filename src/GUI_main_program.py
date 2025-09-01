6be7cb28418f192b31a0f609101e642b19e65fbd
PhysicsRule <olshaaban@hotmail.com>
2025-09-01 04:18:39 -0700

    can change default camera to OakD

--- src/GUI_main_program.py
+++ src/GUI_main_program.py
@@ -174,6 +174,8 @@ class MyGUI(QMainWindow):
         help_action.triggered.connect(self.open_help_window)
         
         # Camera Combo Box
+        # Make default camera 0: Intel Realsense,1:oakDLite etc.
+        self.Select3DCamera.setCurrentIndex(0)
         self.Select3DCamera.currentTextChanged.connect(self.on_camera_selected)
 
         # Folder Options Hidden
@@ -301,9 +303,9 @@ class MyGUI(QMainWindow):
             os.remove(f"{cwd}/cam_img_2.png")
         '''
     # Function to handle camera selection
-    def on_camera_selected(self, camera_name):
-        self.camera_name = camera_name
-        print(camera_name)
+    def on_camera_selected(self, Select3DCamera):
+        self.Select3DCamera = Select3DCamera
+        print(Select3DCamera)
     
     def open_help_window(self):
         self.help_window = HelpWindow()
@@ -973,7 +975,7 @@ class MyGUI(QMainWindow):
         image, color_ranges, min_radius_object, data_output_folder_path  = self.get_settings()
         # config by looking at the camera (remove this from tracking program below)
         # find output folder here instead of 
-        print(self.camera_name)
+        print(self.Select3DCamera)
         if self.tracking_info.types_of_streams_saved =='cd':
         # The color and depth streams need to be aligned for each frame slowing the tracking down
             pipeline = find_and_config_device()
