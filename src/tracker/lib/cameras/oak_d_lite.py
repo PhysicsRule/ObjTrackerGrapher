a1a763c1b9e0391fbb679b2f491ac59a146449fc
Chair <teammoorefamily@gmail.com>
2025-09-01 10:58:31 -0700

    oak stuff. currently broken

--- src/tracker/lib/cameras/oak_d_lite.py
+++ src/tracker/lib/cameras/oak_d_lite.py
@@ -7,6 +7,10 @@ class OakDLite(Camera):
 
     def __init__(self):
         self.pipeline = None
+        self.color_out: Optional[dai.MessageQueue] = None
+        self.left_out: Optional[dai.MessageQueue] = None
+        self.right_out: Optional[dai.MessageQueue] = None
+        self.stereo_out: Optional[dai.MessageQueue] = None
 
     def warm_up_camera(self) -> None:
         pass
@@ -14,15 +18,46 @@ class OakDLite(Camera):
     def find_and_config_device(self):
         self.pipeline = dai.Pipeline()
 
+        color = self.pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+        mono_left = self.pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+        mono_right = self.pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
+        stereo = self.pipeline.create(dai.node.StereoDepth)
+
+        stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.FAST_ACCURACY)
+        stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)  # Align depth to color camera
+        stereo.setOutputSize(640, 480)  # Set output size to match our requested resolution
+
+        color_cam_out = color.requestOutput((640, 480))
+
+        mono_left_out = mono_left.requestOutput((640, 480))
+        mono_right_out = mono_right.requestOutput((640, 480))
+
+        mono_left_out.link(stereo.left)
+        mono_right_out.link(stereo.right)
+
+        self.color_out = color_cam_out.createOutputQueue()
+        self.right_out = mono_right_out.createOutputQueue()
+        self.left_out = mono_left_out.createOutputQueue()
+        self.stereo_out = stereo.depth.createOutputQueue()
+
+        self.pipeline.start()
 
     def find_and_config_device_mult_stream(self, types_of_streams_saved):
-        pass
+        self.find_and_config_device()
 
     def record_bag_file(self, data_output_folder_path, types_of_streams_saved):
         pass
 
     def get_all_frames_color(self) -> Optional[Tuple[Tuple[Any, Any, Any], Any]]:
-        pass
+        color_frame = self.color_out.get()
+        stereo_frame = self.stereo_out.get()
+
+        assert color_frame.validateTransformations()
+        assert stereo_frame.validateTransformations()
+
+        color = color_frame.getCvFrame()
+        depth = stereo_frame.getDepthFrame()
+
 
     def get_all_frames_infrared(self) -> Optional[Tuple[Tuple[Any, Any, Any], Any]]:
         pass
